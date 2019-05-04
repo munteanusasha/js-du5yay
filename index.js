3d9ade8fe3654f9904b81ffac2320dbc0c6541f0
munteanusasha <alexi99.pl@gmail.com>
2019-05-05 02:16:04 +0300

    3 files modified

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>To-Do List App</h1>`;
+
+let item = document.getElementById('add')
+
+function updateList(item){
+
+}
 
 const containerDiv = document.getElementsByClassName('container');
 containerDiv.innerHTML= '<li>hello </li>';
