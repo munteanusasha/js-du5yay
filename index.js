3addbfde0a322aa20e267c68fa9d6a22d802d980
munteanusasha <alexi99.pl@gmail.com>
2019-05-05 02:26:17 +0300

    2 files modified

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>To-Do List App</h1>`;
 
-let item = document.getElementById('add')
+let todolist = [];
+let countid = 0;
 
 function updateList(item){
-
+  item = document.getElementById('add');
+  todolist.push({id: countId ++, listitem: item});
 }
 
 const containerDiv = document.getElementsByClassName('container');
