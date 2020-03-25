04c2eb2e2c43ea8d3aabf76891e145559044b1d4
munteanusasha <alexi99.pl@gmail.com>
2020-03-25 10:42:44 +0000

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Very good book of Javascript
 // https://eloquent-javascript.karmazzin.ru/
 // dd command https://vitux.com/how-to-create-a-bootable-usb-stick-from-the-ubuntu-terminal/
+// https://www.linux.com/training-tutorials/how-burn-iso-usb-drive/
 // Import stylesheets
 import './style.css';
 
