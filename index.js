32909afb7e26deb5e78b5aec89e06a9b8aa0c801
munteanusasha <alexi99.pl@gmail.com>
2020-04-07 18:51:36 +0300

    2 files modified

--- index.js
+++ index.js
@@ -3,18 +3,3 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>To-Do List App</h1>`;
-
-let todolist = [];
-let countid = 0;
-
-function updateList(item){
-  item = document.getElementById('add');
-  todolist.push({id: countId ++, listitem: item});
-}
-
-function showItem(result){
-  result = document.getElementById('search');
-  result.innerHTML = todolist[i];
-}
