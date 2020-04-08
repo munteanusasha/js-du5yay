16a1cf927a017092283c67131db3d8e617cbaeaa
munteanusasha <alexi99.pl@gmail.com>
2020-04-09 01:26:38 +0300

    2 files modified

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 import './style.css';
 
 // Write Javascript code!
+add.addEventListener("click",()=>{
+  list.insertAdjacentHTML('afterbegin','<li class="item-list>'+item.value+'</li>');
+});
+
+
+
