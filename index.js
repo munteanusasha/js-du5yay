feb04ad7fccf6502365ca3bc33a0778f0355dd48
munteanusasha <alexi99.pl@gmail.com>
2020-04-09 02:24:52 +0300

    1 file modified

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ import './style.css';
 
 // Write Javascript code!
 add.addEventListener("click",()=>{
-  list.insertAdjacentHTML('afterbegin','<li class="item-list">'+item.value+'</li>');
+  list.insertAdjacentHTML('afterbegin','<li class="list-item">'+item.value+'</li>');
 });
 
 
-
+let lists = document.getElementsByClassName('list-item');
+search.addEventListener('click',()=>{
+  // console.log('click');
+  for(let i=0; i<lists.length; i++){
+    // console.log(lists[i]);
+    if(item.value==lists[i].text()){
+      console.log(lists[i]);
+    }
+  }
+});
